adf63d68baf1c3831d4bb72af955075062d31da7
AlanWeik <70195860+AlanWeik@users.noreply.github.com>
2023-11-21 21:39:19 +0100

    Deploy Logic Correction

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import VitePluginGhPages from 'vite-plugin-gh-pages';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginGhPages()],
+  plugins: [react()],
+  base: '/https://github.com/AlanWeik/Briefly-AI-Summarizer.git',
 })
+
